54884f14fc9770db1a254e958c46c092b31b9cb9
Максим Лысов <pajzer@gmail.com>
2025-02-27 17:30:46 +0400

    minor fix

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -30,19 +30,18 @@ const ChatPage = () => {
     dispatch(fetchMessagesByToken(token));
   }, [dispatch, token]);
 
-  const renderMessages = () =>
-    filteredMessage.map((message) => (
-      <div
-        id="messages-box"
-        className="chat-messages overflow-auto px-5"
-        key={message.id}
-      >
-        <span className={message.username === username ? 'fw-bold' : 'fw-normal'}>
-          {`${message.username}: `}
-        </span>
-        {message.body}
-      </div>
-    ));
+  const renderMessages = () => filteredMessage.map((message) => (
+    <div
+      id="messages-box"
+      className="chat-messages overflow-auto px-5"
+      key={message.id}
+    >
+      <span className={message.username === username ? 'fw-bold' : 'fw-normal'}>
+        {`${message.username}: `}
+      </span>
+      {message.body}
+    </div>
+  ));
 
   return (
     <Container className="h-100 my-4 overflow-hidden rounded shadow">
